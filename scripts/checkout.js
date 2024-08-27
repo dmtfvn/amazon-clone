388e854f2e054573256e57d72558773b17913259
dmtfvn <dmtfvn@gmail.com>
2024-08-27 13:39:37 +0300

    Create delivery options

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,9 @@
 import { products } from './data/products.js';
 import { cart, removeFromCart } from './data/cart.js';
 import { formatCurrency } from './utils/money.js';
+import { deliveryOptions } from './data/deliveryOptions.js';
+
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 
 let cartSummaryHTML = '';
 
@@ -10,15 +13,29 @@ cart.forEach((cartObj) => {
   let matchPtoduct;
 
   products.forEach((product) => {
-    if (productId === product.id) {
+    if (product.id === productId) {
       matchPtoduct = product;
     }
   });
 
+  const deliveryOptionId = cartObj.deliveryOptionId;
+
+  let deliveryOption;
+
+  deliveryOptions.forEach((option) => {
+    if (option.id === deliveryOptionId) {
+      deliveryOption = option;
+    }
+  });
+
+  const curDate = dayjs();
+  const deliveryDate = curDate.add(deliveryOption.deliveryDays, 'days');
+  const dateString = deliveryDate.format('dddd, MMMM, D');
+
   cartSummaryHTML += `
     <div class="cart-item-container js-cart-item-container-${matchPtoduct.id}">
       <div class="delivery-date">
-        Delivery date: Tuesday, June 21
+        Delivery date: ${dateString}
       </div>
 
       <div class="cart-item-details-grid">
@@ -49,50 +66,48 @@ cart.forEach((cartObj) => {
           <div class="delivery-options-title">
             Choose a delivery option:
           </div>
-          <div class="delivery-option">
-            <input type="radio" checked
-              class="delivery-option-input"
-              name="delivery-option-${matchPtoduct.id}">
-            <div>
-              <div class="delivery-option-date">
-                Tuesday, June 21
-              </div>
-              <div class="delivery-option-price">
-                FREE Shipping
-              </div>
-            </div>
-          </div>
-          <div class="delivery-option">
-            <input type="radio"
-              class="delivery-option-input"
-              name="delivery-option-${matchPtoduct.id}">
-            <div>
-              <div class="delivery-option-date">
-                Wednesday, June 15
-              </div>
-              <div class="delivery-option-price">
-                $4.99 - Shipping
-              </div>
-            </div>
-          </div>
-          <div class="delivery-option">
-            <input type="radio"
-              class="delivery-option-input"
-              name="delivery-option-${matchPtoduct.id}">
-            <div>
-              <div class="delivery-option-date">
-                Monday, June 13
-              </div>
-              <div class="delivery-option-price">
-                $9.99 - Shipping
-              </div>
-            </div>
-          </div>
+          ${deliveryOptionsHTML(matchPtoduct, cartObj)}
         </div>
       </div>
     </div>
   `;
 });
+
+function deliveryOptionsHTML(matchPtoduct, cartObj) {
+  let html = '';
+
+  deliveryOptions.forEach((deliveryOption) => {
+    const curDate = dayjs();
+    const deliveryDate = curDate.add(deliveryOption.deliveryDays, 'days');
+    const dateString = deliveryDate.format('dddd, MMMM, D');
+
+    const priceString = deliveryOption.priceCents === 0
+      ? 'FREE'
+      : `%${formatCurrency(deliveryOption.priceCents)}`
+    ;
+
+    const isChecked = deliveryOption.id === cartObj.deliveryOptionId;
+
+    html += `
+      <div class="delivery-option">
+        <input type="radio"
+          ${isChecked ? 'checked' : ''}
+          class="delivery-option-input"
+          name="delivery-option-${matchPtoduct.id}">
+        <div>
+          <div class="delivery-option-date">
+            ${dateString}
+          </div>
+          <div class="delivery-option-price">
+            $${priceString} - Shipping
+          </div>
+        </div>
+      </div>
+    `;
+  });
+
+  return html;
+}
 
 const checkoutList = document.querySelector('.js-order-summary');
 checkoutList.innerHTML = cartSummaryHTML;
